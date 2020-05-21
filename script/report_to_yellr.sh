f8f86de7dc028cf1357a6ad3995933108f50fde0
Trey Evans <tevans@justplainawful.lan>
2020-05-21 10:33:07 -0400

    Spell 'enterprise' correctly.

--- script/report_to_yellr.sh
+++ script/report_to_yellr.sh
@@ -5,4 +5,4 @@ regex='refs/heads/(.*)'
 SIMPLE_BRANCH=${BASH_REMATCH[1]}
 echo "Will post result: {\"project\":\"hbx_enterprise\",\"branch\":\"$SIMPLE_BRANCH\",\"sha\":\"$SHA\",\"status\":\"$STATUS\"}"
 
-curl -H "Content-Type: application/json" -H "X-API-Key: $YELLR_KEY" -X POST $YELLR_URL -d "{\"project\":\"hbx_enteprise\",\"branch\":\"$SIMPLE_BRANCH\",\"sha\":\"$SHA\",\"status\":\"$STATUS\"}"
+curl -H "Content-Type: application/json" -H "X-API-Key: $YELLR_KEY" -X POST $YELLR_URL -d "{\"project\":\"hbx_enterprise\",\"branch\":\"$SIMPLE_BRANCH\",\"sha\":\"$SHA\",\"status\":\"$STATUS\"}"
